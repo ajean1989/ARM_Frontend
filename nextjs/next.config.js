7e580889418922252bef46dac3b9d715cb20b95a
adrien <adrien.jean1989@gmail.com>
2024-05-09 19:05:22 +0200

    https ok

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -2,4 +2,7 @@ module.exports = {
     // ... rest of the configuration.
     output: "standalone",
     // productionBrowserSourceMaps: true,
+    resolve: {
+      extensions: ['.js', '.jsx']
+    }
   };
